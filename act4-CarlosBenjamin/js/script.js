448299066f56e1780af4b8862a57b96b037a2d75
DiazPedroza_CarlosBenjamin <20223tn048@utez.edu.mx>
2023-05-16 23:07:54 -0600

    Actulize la carpeta

---
+++ act4-CarlosBenjamin/js/script.js
@@ -0,0 +1,9 @@
+function ActualizarFondo() {
+    var body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "red";
+}
+
+function Cambiartexto(){
+    var Texto = document.getElementById("miTexto");
+    Texto.style.backgroundColor = "azul";
+}
