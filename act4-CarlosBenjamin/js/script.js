9711b3570731abee16cba4475d212c6b43aaf97b
DiazPedroza_CarlosBenjamin <20223tn048@utez.edu.mx>
2023-05-18 10:45:35 -0600

    modifique mi carpeta

--- act4-CarlosBenjamin/js/script.js
+++ act4-CarlosBenjamin/js/script.js
@@ -7,3 +7,18 @@ function Cambiartexto(){
     var Texto = document.getElementById("miTexto");
     Texto.style.backgroundColor = "azul";
 }
+
+function cambiarImagen() {
+    var imagen = document.getElementById("imagen");
+    var rutaImagenActual = imagen.src;
+    var nombreImagenActual = rutaImagenActual.substring(rutaImagenActual.lastIndexOf("/") + 1);
+  
+    if (nombreImagenActual === "porscheimg1.png") {
+      imagen.src = "img/porschemg2.png";
+      imagen.alt = "Imagen 2";
+    } else {
+      imagen.src = "img/porscheimg1.png";
+      imagen.alt = "Imagen 1";
+    }
+  }
+  
